a13b9a22580fbbcbb77e5a0833954cb56624001f
VladimirS01 <vbnm1993@yandex.ru>
2023-12-06 18:56:54 +0500

    Initial app

---
+++ task-management-api/src/test/java/ru/solarev/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solarev.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
